dbd9487bc041f98bb09fd0545b3a79d244efc31f
--global <neeraj.kaushiktechy@gmail.com>
2025-05-01 18:20:34 +0530

    functionalities from frontend added till pagination and edit delete

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const nurseRoutes = require('./routes/nurseRoutes.js');
 const aiRoutes = require('./routes/airoutes.js');
+const adminRoutes = require('./routes/adminRoute.js')
 const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const port = process.env.PORT || 3000;
-require('dotenv').config();
 // Middleware
-app.use(bodyParser.json());
+app.use(cookieParser())
+app.use(express.json());
+
+const corsOptions = {
+  origin: 'http://localhost:5173',  // The origin of your frontend (React app)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true
+};
+
+// Enable CORS with the specified options
+app.use(cors(corsOptions));
+
 
 app.use('/api', nurseRoutes);
 app.use('/api',aiRoutes);
+app.use('/api/admin',adminRoutes)
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
