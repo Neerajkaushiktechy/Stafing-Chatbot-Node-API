c29a7da2540ad92a4af0bc3342e68c28e485241d
--global <neeraj.kaushiktechy@gmail.com>
2025-05-06 17:28:44 +0530

    calendar and search functionality added

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {admin_login,get_nurse_by_id, add_facility, logout, get_facility, get_nurses, add_nurse, add_nurse_type, get_nurse_type, edit_facility, get_facility_by_id, delete_facility,delete_nurse, delete_service, get_nurse_types, delete_nurse_type, edit_nurse_type, edit_nurse} = require('../controller/admin_controller');
+const {admin_login,get_nurse_by_id, add_facility, logout, get_facility, get_nurses, add_nurse, add_nurse_type, get_nurse_type, edit_facility, get_facility_by_id, delete_facility,delete_nurse, delete_service, get_nurse_types, delete_nurse_type, edit_nurse_type, edit_nurse, get_shifts} = require('../controller/admin_controller');
 const jwt = require('jsonwebtoken');
 const authenticateToken = require('../middleware/authenticateToken');
 
@@ -8,7 +8,6 @@ router.post("/login",admin_login)
 router.get('/verify-token', authenticateToken)
 router.post('/add-facility', add_facility)
 router.put('/edit-facility/:id', edit_facility)
-router.get('/check-login', authenticateToken)
 router.post('/logout',logout)
 router.get('/get-facility',get_facility)
 router.get('/get-nurses', get_nurses)
@@ -24,4 +23,5 @@ router.delete('/delete-service/:id/:role',delete_service)
 router.get('/get-nurse-types', get_nurse_types)
 router.delete('/delete-nurse-type/:id',delete_nurse_type)
 router.put('/edit-nurse-type/:id',edit_nurse_type)
+router.get('/get-shifts', get_shifts)
 module.exports = router;
